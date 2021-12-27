2c1b42f4d40bf5d33c7d84e35bf3418dff0e512f
Rohin Manvi <rohinmanvi@macbook-pro-2.lan>
2021-12-26 22:53:14 -0800

    added image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import logo from './logo.svg';
+import logo from './testmap.png';
 import './App.css';
 import mixpanel from 'mixpanel-browser';
 import { v4 as uuidv4 } from 'uuid';
@@ -15,20 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} alt="logo" />
     </div>
   );
 }
